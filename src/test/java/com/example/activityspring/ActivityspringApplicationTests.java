3e1ba75918c8e0f8f03f2fac48efb4f50e8430bd
zri <cedzreola@gmail.com>
2025-03-12 22:12:32 +0800

    Added new files

---
+++ src/test/java/com/example/activityspring/ActivityspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activityspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
